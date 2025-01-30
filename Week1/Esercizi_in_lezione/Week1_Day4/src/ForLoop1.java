e11b11712df46b849e14c4ba2f786c2a513dfdce
formidablae <81068781+formidablae@users.noreply.github.com>
2025-01-30 13:06:30 +0100

    feat: aggiunta di esercizi di Week 1 Day 4

---
+++ Week1/Esercizi_in_lezione/Week1_Day4/src/ForLoop1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ForLoop1 {
+    public static void main(String[] args) {
+        System.out.println("Prima del ciclo for");
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 1; i < 100; i++) {
+            System.out.println("Durante il ciclo for. i = " + i);
+            if (i % 10 == 0) {
+                System.out.print("Vuoi interrompere il ciclo: 'S' per indicare Sì: ");
+                String risp = scanner.next();
+                if (risp.equals("S")) break;
+            }
+        }
+        System.out.println("Sono uscito dal ciclo for");
+    }
+}
