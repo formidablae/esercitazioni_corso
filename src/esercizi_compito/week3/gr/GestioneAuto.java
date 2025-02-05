2171b1d52ba957b74ef062b47eb4510810633666
R-Giuseppe <peppe.rosace@gmail.com>
2025-02-05 12:28:40 +0100

    Ho svolto l'esercizio GR

--- src/esercizi_compito/week3/gr/GestioneAuto.java
+++ src/esercizi_compito/week3/gr/GestioneAuto.java
@@ -21,7 +21,7 @@ Istruzioni:
     - Stampare tutte le auto.
     - Trovare e stampare:
         - L'auto più costosa.
-        - Incrementare il prezzo di ciascuna auto i 10% (cioè moltiplicare il prezzo precedente per 1.1).
+        - Incrementare il prezzo di ciascuna auto del 10% (cioè moltiplicare il prezzo precedente per 1.1).
         - Le auto prodotte dopo il 2020.
 
 Elenco auto:
@@ -41,10 +41,64 @@ Auto prodotte dopo il 2020:
 
 class Auto {
     // Implementare qui la soluzione
+    private String marca;
+    private String modello;
+    private int anno;
+    private double prezzo;
+
+    public Auto(String marca, String modello, int anno, double prezzo){
+        this.marca = marca;
+        this.modello = modello;
+        this.anno = anno;
+        this.prezzo = prezzo;
+    }
+    public String getMarca(){
+        return marca;
+    }
+    public String getModello(){
+        return modello;
+    }
+    public int getAnno(){
+        return anno;
+    }
+    public double getPrezzo(){
+        return prezzo;
+    }
+    public void setPrezzo(double prezzo){
+        this.prezzo = prezzo;
+    }
+    public String toString(){
+        return "Marca: " + marca + "\n Modello: " + modello + "\n  Anno: " + anno + "\n   Prezzo: " + prezzo;
+    }
 }
 
 public class GestioneAuto {
     public static void main(String[] args) {
         // Implementare qui la soluzione
+        Auto auto1 = new Auto("BMW", "Serie 3", 2018, 27000.0);
+        Auto auto2 = new Auto("Audi", "A4", 2021, 32000.0);
+        Auto auto3 = new Auto("Ford", "Focus", 2019, 18000.0);
+        Auto auto4 = new Auto("Mercedes", "Classe C", 2022, 40000.0);
+        Auto auto5 = new Auto("Toyota", "Corolla", 2020, 21000.0);
+
+        Auto[] elencoAuto = {auto1, auto2, auto3, auto4, auto5};
+        Auto piuCostosa = elencoAuto[0];
+        System.out.println("Questo è l'elenco delle auto: ");
+        for (int i = 0; i < elencoAuto.length ; i++) {
+            double prezzo2 = elencoAuto[i].getPrezzo() * 1.1;
+            elencoAuto[i].setPrezzo(prezzo2);
+            System.out.println("Auto " + (i + 1) + "\n" + elencoAuto[i]);
+            if (elencoAuto[i].getPrezzo() > piuCostosa.getPrezzo()){
+                piuCostosa = elencoAuto[i];
+            }
+        }
+        System.out.println("L'auto più costosa è la: \n" + piuCostosa + "\n");
+
+        for (int i = 0; i < elencoAuto.length; i++) {
+            if (elencoAuto[i].getAnno() > 2020){
+                System.out.println("Questa auto è stata prodotta dopo il 2020: \n" + elencoAuto[i]);
+            }
+        }
     }
+
 }
