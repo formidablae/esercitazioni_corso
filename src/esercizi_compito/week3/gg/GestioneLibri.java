885441e135894dfa32493eefa9d35d444cc04b8c
Gabriele Gaita <gabrielegaita001@gmail.com>
2025-02-06 12:03:03 +0100

    Esecuzione esercizio GG

--- src/esercizi_compito/week3/gg/GestioneLibri.java
+++ src/esercizi_compito/week3/gg/GestioneLibri.java
@@ -42,11 +42,91 @@ Libri pubblicati dopo il 2015:
 
 class Libro {
     // Implementare la soluzione qui
+    private String titolo;
+    private String autore;
+    private int annoPubblicazione;
+    private double prezzo;
 
+    public Libro(String titolo, String autore, int annoPubblicazione, double prezzo) {
+        this.titolo = titolo;
+        this.autore = autore;
+        this.annoPubblicazione = annoPubblicazione;
+        this.prezzo = prezzo;
+    }
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public String getAutore() {
+        return autore;
+    }
+
+    public void setAutore(String autore) {
+        this.autore = autore;
+    }
+
+    public int getAnnoPubblicazione() {
+        return annoPubblicazione;
+    }
+
+    public void setAnnoPubblicazione(int annoPubblicazione) {
+        this.annoPubblicazione = annoPubblicazione;
+    }
+
+    public double getPrezzo() {
+        return prezzo;
+    }
+
+    public void setPrezzo(double prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    @Override
+    public String toString() {
+        return "Libro{" +
+                "titolo='" + titolo + '\'' +
+                ", autore='" + autore + '\'' +
+                ", annoPubblicazione=" + annoPubblicazione +
+                ", prezzo=" + prezzo + "€" +
+                '}';
+    }
 }
 
 public class GestioneLibri {
     public static void main(String[] args) {
         // Implementare la soluzione qui
+
+        Libro libro1 = new Libro("Il Signore degli Anelli", "J.R.R. Tolkien", 1954, 25.99);
+        Libro libro2 = new Libro("1984", "George Orwell", 1949, 15.50);
+        Libro libro3 = new Libro("Il Codice da Vinci", "Dan Brown", 2003, 19.99);
+        Libro libro4 = new Libro("Sapiens", "Yuval Noah Harari", 2015, 22.00);
+        Libro libro5 = new Libro( "La ragazza del treno", "Paula Hawkins", 2018, 18.90);
+
+        Libro[] libri = {libro1, libro2, libro3, libro4, libro5};
+
+        Libro piuCostoso = libri[0];
+        double somma = 0;
+        for(int i = 0; i < libri.length; i++){
+            System.out.println(libri[i]);
+            if(libri[i].getPrezzo() > piuCostoso.getPrezzo()){
+                piuCostoso = libri[i];
+            }
+            somma += libri[i].getPrezzo();
+        }
+        System.out.println("Prezzo medio dei libri: "+ somma / libri.length);
+        System.out.println("Libro piu costoso: "+ piuCostoso);
+
+        System.out.println("I libri usciti dopo il 2015 sono\n");
+        for(int i = 0; i < libri.length; i++){
+            if(libri[i].getAnnoPubblicazione() > 2015){
+                System.out.println(libri[i]);
+            }
+        }
+
     }
 }
