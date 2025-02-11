b7f7aec1a494d2bdf707707d59370355ed166e96
formidablae <81068781+formidablae@users.noreply.github.com>
2025-02-11 10:56:07 +0100

    feat: exe oop final

---
+++ src/esercizi_in_lezione/week4/day2/Pianeta.java
@@ -0,0 +1,9 @@
+package esercizi_in_lezione.week4.day2;
+
+public class Pianeta {
+    public final String NOME = "Terra";
+
+    public String toString() {
+        return "Pianeta, nome: " + NOME;
+    }
+}
