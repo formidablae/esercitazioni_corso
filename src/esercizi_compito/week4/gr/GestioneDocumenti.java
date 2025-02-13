0e908861917ca592bf004355e82d6bcc6737eca4
formidablae <81068781+formidablae@users.noreply.github.com>
2025-02-13 10:13:16 +0100

    feat: exe oop, intefacce, metodi string, stringbuilder gr

---
+++ src/esercizi_compito/week4/gr/GestioneDocumenti.java
@@ -0,0 +1,17 @@
+package esercizi_compito.week4.gr;
+
+/*
+1. Creare una classe Documento, con titolo e contenuto.
+2. Creare una classe FileDigitale non istanziabile, che estende Documento, aggiunge informazioni sul formato e definisce un metodo per leggere il contenuto.
+3. Creare una classe DocumentoPDF che eredita da FileDigitale, ha un metodo specifico per proteggere il file (void proteggi(Documento file).
+4. Creare le interfacce IModificabile e IStampabile che definiscono operazioni/metodi da eseguire sui dei file.
+5. Creare una classe DocumentoWord che eredita da FileDigitale e implementa entrambe le interfacce.
+6. In main in GestioneDocumenti creare un array di documenti, iterare con un ciclo for e manipolare qualche campo/qualche valore.
+7. Uso qualche metodo di Stringhe e usare StringBuilder per concatenare stringhe
+ */
+
+public class GestioneDocumenti {
+    public static void main(String[] args) {
+        // implementare qui punto 6 e forse anche 7
+    }
+}
