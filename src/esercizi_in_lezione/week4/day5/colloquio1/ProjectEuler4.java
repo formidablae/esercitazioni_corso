92ed2fa2b61169e081e08ee2556457e59c87126c
scarf89 <phoenixserviceoff@gmail.com>
2025-02-14 13:03:03 +0100

    ho svolto esercizio collooquio

--- src/esercizi_in_lezione/week4/day5/colloquio1/ProjectEuler4.java
+++ src/esercizi_in_lezione/week4/day5/colloquio1/ProjectEuler4.java
@@ -13,5 +13,56 @@ Trovare il più grande numero palindromo ottenuto dal prodotto di due numeri a 3
 public class ProjectEuler4 {
     public static void main(String[] args) {
         // scrivere qui la soluzione
+
+        int palindromoMax=0;
+
+
+        for(int i=100; i<1000; i++){
+
+            for(int j=100; j<1000; j++){
+
+                int prodotto = i*j;
+
+                if(prodotto>palindromoMax && isPalindromo(prodotto)) {
+                    palindromoMax=prodotto;
+                }
+
+            }
+
+        }
+
+        System.out.println(palindromoMax);
+
+    }
+
+
+    public static boolean isPalindromo(int numero){
+
+        String numeroStr = ""+numero;
+        String numeroContr = conversione(numeroStr);
+
+        if (numeroStr.equals(numeroContr)) {
+
+            return true;
+        }
+        else {
+
+            return false;
+
+        }
+
+    }
+
+    public static String conversione(String imput){
+
+         String risultato = "";
+         for(int i=imput.length()-1; i>=0; i--){
+
+             risultato=risultato+imput.charAt(i);
+
+         }
+            return risultato;
+
+
     }
 }
