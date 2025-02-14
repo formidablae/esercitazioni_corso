3aa11e5b71bedb67600ab4b1e6ac882ee40991b4
formidablae <81068781+formidablae@users.noreply.github.com>
2025-02-14 10:25:47 +0100

    feat: exe 2 prova tecnica

---
+++ src/esercizi_in_lezione/week4/day5/colloquio1/ProjectEuler2.java
@@ -0,0 +1,17 @@
+package esercizi_in_lezione.week4.day5.colloquio1;
+
+/*
+https://projecteuler.net/problem=2
+
+Problema: Somma dei termini pari della sequenza di Fibonacci
+Ogni nuovo termine nella sequenza di Fibonacci è generato sommando i due termini precedenti. Iniziando con 1 e 2, i primi 10 termini della sequenza sono:
+
+1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
+
+Considerando i termini della sequenza di Fibonacci che non superano quattro milioni, trovare la somma di quelli con valore pari.
+ */
+public class ProjectEuler2 {
+    public static void main(String[] args) {
+        // scrivere qui la soluzione
+    }
+}
