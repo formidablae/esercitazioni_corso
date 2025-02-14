3c98ca85c1e602fbdd0a968b039f33d49dd17b42
formidablae <81068781+formidablae@users.noreply.github.com>
2025-02-14 12:22:56 +0100

    feat: exe 4 prova tecnica

---
+++ src/esercizi_in_lezione/week4/day5/colloquio1/ProjectEuler4.java
@@ -0,0 +1,17 @@
+package esercizi_in_lezione.week4.day5.colloquio1;
+
+/*
+https://projecteuler.net/problem=4
+
+Problema: Palindromo più grande da due numeri a 3 cifre
+Un numero palindromo è un numero che si legge uguale sia da sinistra a destra che da destra a sinistra.
+
+Il palindromo più grande ottenuto dal prodotto di due numeri a 2 cifre è 9009 = 91 × 99.
+
+Trovare il più grande numero palindromo ottenuto dal prodotto di due numeri a 3 cifre.
+ */
+public class ProjectEuler4 {
+    public static void main(String[] args) {
+        // scrivere qui la soluzione
+    }
+}
