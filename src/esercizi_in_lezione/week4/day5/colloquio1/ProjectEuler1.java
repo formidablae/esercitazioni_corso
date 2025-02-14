84c49134eb786ca39f03cc09b203ac97508e9a14
formidablae <81068781+formidablae@users.noreply.github.com>
2025-02-14 10:09:20 +0100

    feat: exe prova tecnica

---
+++ src/esercizi_in_lezione/week4/day5/colloquio1/ProjectEuler1.java
@@ -0,0 +1,15 @@
+package esercizi_in_lezione.week4.day5.colloquio1;
+
+/*
+https://projecteuler.net/problem=1
+
+Problema: Somma dei multipli di 3 o 5
+Se elenchiamo tutti i numeri naturali inferiori a 10 che sono multipli di 3 o 5, otteniamo: 3, 5, 6 e 9. La somma di questi multipli è 23.
+
+Scrivere un programma Java che trovi la somma di tutti i multipli di 3 o 5 al di sotto di 1000.
+ */
+public class ProjectEuler1 {
+    public static void main(String[] args) {
+        // scrivere qui la soluzione
+    }
+}
