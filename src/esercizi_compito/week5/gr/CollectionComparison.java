fe71027434faccda2e8436d9346e0da02c3a505d
formidablae <81068781+formidablae@users.noreply.github.com>
2025-02-21 10:12:02 +0100

    feat: exe hashset gr

---
+++ src/esercizi_compito/week5/gr/CollectionComparison.java
@@ -0,0 +1,15 @@
+package esercizi_compito.week5.gr;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class CollectionComparison {
+    public static void main(String[] args) {
+        // 10, 20, 30, 40, 50, 10, 25, 50
+        // 1. creare un hashset composto da questi numeri
+        // 2. creare un arraylist composto da questi numeri
+        // 3. dire cosa ci aspettiamo di avere nel risultato di ciascuno
+        // 4. stampare le due strutture dati e verificare i risultati
+
+    }
+}
