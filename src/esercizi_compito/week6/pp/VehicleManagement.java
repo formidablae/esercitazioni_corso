0ed579eda35039562fe96d8067b11e4b70dbf97a
formidablae <81068781+formidablae@users.noreply.github.com>
2025-02-24 13:07:55 +0100

    feat: exe pp argomenti recenti

---
+++ src/esercizi_compito/week6/pp/VehicleManagement.java
@@ -0,0 +1,25 @@
+package esercizi_compito.week6.pp;
+
+/*
+1. Scrivere un'intefaccia IIdentifiable con metodo getIdentifier
+2. Crea una classe Vehicle, astratta che implementa IIdentifiable e ha:
+ - brand
+ - year
+3. Creare Car e Bike che estendono Vehicle
+ - brand
+ - year
+4. Implementare equals e hashCode in Vehicle, Car e Bike
+5. Implementare il metodo getIdentifier in Vehicle che restituisca: brand + "-" + year
+6. Implementare il metodo getIdentifier in Car che restituisca: l'identificatore della classe padre + "-Car"
+7. Implementare il metodo getIdentifier in Bike che restituisca: l'identificatore della classe padre + "-Bike"
+In VehicleManagement, main:
+8. creare 5 Car, 3 Bike, una variabile Car null
+9. creare un HashSet di Vehicle e aggiungere i 5 Car, 3 Bike e la variabile Car null
+10. stampare il numero di Vehicle unici
+11. stampare il numero di Car uniche
+12. stampare il numero di Bike uniche
+13. stampare il numero di Vehicle null
+14. per ogni Vehicle, stampare l'identificatore
+ */
+public class VehicleManagement {
+}
