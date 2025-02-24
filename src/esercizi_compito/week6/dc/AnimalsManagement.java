b5e62faaab1cde8f0a876487fd2099a48487e1d9
formidablae <81068781+formidablae@users.noreply.github.com>
2025-02-24 12:41:01 +0100

    feat: exe dc argomenti recenti

---
+++ src/esercizi_compito/week6/dc/AnimalsManagement.java
@@ -0,0 +1,19 @@
+package esercizi_compito.week6.dc;
+
+/*
+1. Definire un'interfaccia IAnimal con un metodo makeSound().
+2. Creare una classe Dog che implementa l'interfaccia Animal.
+3. Definire un costruttore nella classe Dog che accetta il nome e l'età del cane.
+4. Implementare il metodo makeSound() in Dog, che stampa un messaggio personalizzato.
+5. Implementare il metodo equals() in Dog per confrontare oggetti in base al nome e all'età.
+Nel metodo main di AnimalsManagement:
+6. Utilizzare HashSet<Dog> per memorizzare cani unici ed evitare duplicati.
+7. Verificare se due istanze Dog sono uguali, aggiungendole all'HashSet e controllando la dimensione.
+8. Utilizzare instanceof per verificare se un oggetto è un'istanza di Dog, gestendo anche il caso null.
+ */
+
+public class AnimalsManagement {
+    public static void main(String[] args) {
+        //
+    }
+}
