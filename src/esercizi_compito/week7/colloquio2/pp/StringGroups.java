4b2be90eed1d690ec618165879b799bb3bdf63a2
paolo9688 <paolo.pz1@gmail.com>
2025-03-04 10:53:36 +0100

    svolto esercizio colloquio pp

--- src/esercizi_compito/week7/colloquio2/pp/StringGroups.java
+++ src/esercizi_compito/week7/colloquio2/pp/StringGroups.java
@@ -20,6 +20,7 @@ Output: {{"a"}}
 */
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class StringGroups {
     public static void main(String[] args) {
@@ -28,8 +29,8 @@ public class StringGroups {
         String[] strs3 = {"a"};
 
         String[][] risultato1 = solve(strs1);
-        String[][] risultato2 = solve(strs1);
-        String[][] risultato3 = solve(strs1);
+        String[][] risultato2 = solve(strs2);
+        String[][] risultato3 = solve(strs3);
 
         System.out.println("strs1: " + Arrays.toString(strs1) + "; Risultato 1: " + toStringNestedArray(risultato1));
         System.out.println("strs2: " + Arrays.toString(strs2) + "; Risultato 2: " + toStringNestedArray(risultato2));
@@ -50,7 +51,23 @@ public class StringGroups {
 
     public static String[][] solve(String[] strs) {
         // TODO: code the solution here
-        String[][] risultato = {{""}};
-        return risultato;
+
+        HashMap<String, String[]> map = new HashMap<>();
+
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String sortedStr = new String(chars);
+            if (map.containsKey(sortedStr)) {
+                String[] group = map.get(sortedStr);
+                String[] newGroup = Arrays.copyOf(group, group.length + 1);
+                newGroup[group.length] = str;
+                map.put(sortedStr, newGroup);
+            } else {
+                map.put(sortedStr, new String[]{str});
+            }
+        }
+
+        return map.values().toArray(new String[0][]);
     }
 }
