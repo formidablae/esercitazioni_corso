6fbf0c52adbd8aa82578a80ac941bc201654d0bb
Dennis Ciccone <dennisciccone02@gmail.com>
2025-03-05 10:31:57 +0100

    Svolto esercizio sulle date in week7.dc

--- src/esercizi_compito/week7/dc/DateExe.java
+++ src/esercizi_compito/week7/dc/DateExe.java
@@ -1,5 +1,9 @@
 package esercizi_compito.week7.dc;
 
+import java.time.OffsetDateTime;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
 /*
  1. Creare un'applicazione Java che utilizzi Date e OffsetDateTime.
  2. Convertire una data (ad esempio la data di oggi) in formato ISO 8601.
@@ -11,6 +15,29 @@ package esercizi_compito.week7.dc;
 public class DateExe {
     public static void main(String[] args) {
         // implementare qui la soluzione
+        String dateStr = "2025-03-20T10:15:30+01:00";
+        OffsetDateTime date = OffsetDateTime.parse(dateStr);
+        System.out.println(date);
+
+        //offsetDateTime adesso
+        OffsetDateTime now = OffsetDateTime.now();
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String formattedDateTime = date.format(formatter);
+        System.out.println("\nData generica: " + formattedDateTime);
+
+        DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        String formattedDateTime2 = date.format(formatter2);
+        System.out.println("\nData Americana: " + formattedDateTime2);
+
+        OffsetDateTime newDate = date.plusDays(2).minusHours(3);
+        System.out.println("\nData modificata: " + newDate);
+
+        ZonedDateTime cambioFuso = newDate.atZoneSameInstant(java.time.ZoneId.of("Europe/Rome"));
+        System.out.println("\nData modificata in Italia: " + cambioFuso);
+
+        long daysBetween = now.getDayOfYear() - date.getDayOfYear();
+        System.out.println("\nDifferenza in giorni: " + daysBetween);
 
     }
 }
