3af91a3cb42ec12536c577f85cf9d8ac928365cc
formidablae <81068781+formidablae@users.noreply.github.com>
2025-03-05 10:17:41 +0100

    feat: exe dc

---
+++ src/esercizi_compito/week7/dc/DateExe.java
@@ -0,0 +1,16 @@
+package esercizi_compito.week7.dc;
+
+/*
+ 1. Creare un'applicazione Java che utilizzi Date e OffsetDateTime.
+ 2. Convertire una data (ad esempio la data di oggi) in formato ISO 8601.
+ 3. Formattare un OffsetDateTime in diversi formati (ad esempio data americana, data italiana, ecc.).
+ 4. Eseguire operazioni con OffsetDateTime (come ad esempio aggiungere 2 giorni, sottrarre 3 ore)
+ 5. Cambiare il fuso orario di un OffsetDateTime (ad esempio da UTC a UTC+2).
+ 6. Calcolare la differenza in giorni tra due OffsetDateTime.
+ */
+public class DateExe {
+    public static void main(String[] args) {
+        // implementare qui la soluzione
+
+    }
+}
