7f4aa9b97d4dba34a3ca844993cad33635d0d399
formidablae <81068781+formidablae@users.noreply.github.com>
2025-03-07 12:40:51 +0100

    test: exe testing dc

---
+++ src/esercizi_compito/week7/dc2/GestoreUtenti.java
@@ -0,0 +1,27 @@
+package esercizi_compito.week7.dc2;
+
+import java.util.HashSet;
+
+public class GestoreUtenti {
+    private HashSet<String> utenti;
+
+    public GestoreUtenti() {
+        utenti = new HashSet<>();
+    }
+
+    public void aggiungiUtente(String nome) {
+        utenti.add(nome);
+    }
+
+    public void rimuoviUtente(String nome) {
+        utenti.remove(nome);
+    }
+
+    public boolean verificaEsistenzaUtente(String nome) {
+        return utenti.contains(nome);
+    }
+
+    public boolean ricercaUtente(String nome) {
+        return utenti.contains(nome);
+    }
+}
---
+++ test/esercizi_compito/week7/dc2/GestoreUtentiTest.java
@@ -0,0 +1,18 @@
+package esercizi_compito.week7.dc2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/*
+Scrivere dei test per verificare che:
+- un utente venga aggiunto correttamente.
+- un utente venga rimosso correttamente.
+- un utente inesistente non venga rimosso.
+- la verificaEsistenzaUtente di un utente esistente ritorni true.
+- la verificaEsistenzaUtente di un utente non esistente ritorni false.
+- la ricercaUtente di un utente esistente ritorni true.
+- la ricercaUtente di un utente non esistente ritorni false.
+ */
+
+class GestoreUtentiTest {
+    // implementare qui i test
+}
