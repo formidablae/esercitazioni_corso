2bf2b917395c40691bbd0939d867af244e0759be
formidablae <81068781+formidablae@users.noreply.github.com>
2025-03-11 10:36:10 +0100

    feat: exe big int, big decimal, math, ternary, test pp

---
+++ src/esercizi_compito/week8/pp/OperazioniMatematiche.java
@@ -0,0 +1,23 @@
+package esercizi_compito.week8.pp;
+
+/**
+ * 1. Creare un metodo che sommi due numeri di tipo BigInteger.
+ * Il metodo deve restituire il risultato come BigInteger.
+ *
+ * 2. Creare un metodo che moltiplichi due numeri di tipo BigDecimal.
+ * Il metodo deve restituire il risultato come BigDecimal.
+ *
+ * 3. Creare un metodo che calcoli la radice quadrata di un numero double utilizzando Math.sqrt().
+ * Il metodo deve restituire un valore double.
+ *
+ * 4. Creare un metodo che restituisca il maggiore tra due numeri interi utilizzando l'operatore ternario ? :.
+ * Il metodo deve restituire un valore int.
+ *
+ * 5. Creare una classe di test MathOperationsTest.
+ * Implementare test per tutti i metodi sopra definiti.
+ *
+ * Nota: Per la radice quadrata, tollerare un errore minimo con un valore di delta (es. 0.0001).
+ */
+public class OperazioniMatematiche {
+    // TODO: implementare i metodi richiesti
+}
