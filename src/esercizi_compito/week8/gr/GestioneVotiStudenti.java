6aa90759d8aa98c211c742b921a85110b8fc553c
formidablae <81068781+formidablae@users.noreply.github.com>
2025-03-11 10:11:52 +0100

    feat: exe gr strutture dati

---
+++ src/esercizi_compito/week8/gr/GestioneVotiStudenti.java
@@ -0,0 +1,22 @@
+package esercizi_compito.week8.gr;
+
+public class GestioneVotiStudenti {
+    public static void main(String[] args) {
+        // usare simulaVotoRandom() per generare voti casuali
+        // 1. creare un array tradizionale con i nomi degli studenti
+
+        // 2. creare un HashMap con chiavi stringhe e valori double
+        // per associare studenti e loro voti medi
+
+        // 3. creare un ArrayList di tutti i voti dati
+
+        // 4. creare un HashSet di tutti i voti (univoci) dati
+
+        // 5. inserire in un LinkedList i voti nell'ordine inverso
+
+    }
+
+    public static int simulaVotoRandom() {
+        return (int) (Math.random() * 10) + 1;
+    }
+}
