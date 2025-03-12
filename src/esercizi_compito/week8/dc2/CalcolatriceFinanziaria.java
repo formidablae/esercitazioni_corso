eeb5ee7b207f8d479fe9485003fab88c9eb1ca88
formidablae <81068781+formidablae@users.noreply.github.com>
2025-03-12 10:19:07 +0100

    feat: operazione matematiche, bigint, bigdec

---
+++ src/esercizi_compito/week8/dc2/CalcolatriceFinanziaria.java
@@ -0,0 +1,64 @@
+package esercizi_compito.week8.dc2;
+
+/*
+1. scrivere una funzione static calcolaInteresse(...) che:
+- prende in input un BigInteger capitale,
+- prende in input un BigDecimal tasso,
+- prende in input un int anni
+- la formula per il calcolo dell'interesse è:
+    totaleInteresse = capitale * ((1 + tasso) ^ anni)
+- restituire il valore dell'interesse come BigDecimal
+
+2. scrivere una funzione static sommaBigIntegers(...) che:
+- prende in input due BigInteger
+- restituire la somma dei due BigInteger
+
+3. scrivere una funzione static sommaBigIntegers(...) che:
+- prende in input un array di BigInteger
+- restituire la somma dei BigInteger nell'array
+- se l'array è vuoto, restituire 0
+
+4. scrivere una funzione fa la divisione di BigDecimal in modo sicuro:
+- nome funzione static divisioneSicuraBigDecimal(...)
+- prende in input un BigDecimal dividendo
+- prende in input un BigDecimal divisore
+- se il divisore è 0, lanciare un'eccezione
+- restituire il risultato della divisione
+
+5. scrivere una funzione che calcola la rata mensile di un mutuo
+dato importo capitale del mutuo, tasso di interesse e numero di anni:
+- nome funzione static calcolaRataMutuo(...)
+- prende in input un BigDecimal capitale
+- prende in input un BigDecimal tasso di interesse
+- prende in input un int anni
+- restituire il valore della rata mensile come BigDecimal
+- la formula per il calcolo della rata mensile è:
+    rataMensile = (capitale * tasso) / (1 - ((1 + tasso) ^ (-anni)))
+
+6. nel main, chiamare la funzione calcolaInteresse(...) con i seguenti valori:
+- capitale = 200000
+- tasso = 2.7% (0.027)
+- anni = 20
+- stampare il risultato della funzione calcolaInteresse(...)
+
+7. nel main, chiamare la funzione calcolaRataMutuo(...) con i seguenti valori:
+- capitale = 200000
+- tasso = 2.7% (0.027)
+- anni = 20
+- stampare il risultato della funzione calcolaRataMutuo(...)
+
+8. nel main, chiamare la funzione sommaBigIntegers(...) con i seguenti valori:
+- 100000000000
+- 200000000000
+- stampare il risultato della funzione sommaBigIntegers(...)
+
+9. nel main, chiamare la funzione divisioneSicuraBigDecimal(...) con i seguenti valori:
+- dividendo = 1000000000000.1
+- divisore = 2.99999999999
+- stampare il risultato della funzione divisioneSicuraBigDecimal(...)
+ */
+public class CalcolatriceFinanziaria {
+    // implementare qui le funzionalità richieste
+    public static void main(String[] args) {
+    }
+}
