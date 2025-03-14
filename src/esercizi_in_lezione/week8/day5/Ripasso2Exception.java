6d1e69679afec4be7f19c463f98186e9c912a5c7
R-Giuseppe <peppe.rosace@gmail.com>
2025-03-14 14:49:16 +0100

    Ho svolto exe GR

--- src/esercizi_in_lezione/week8/day5/Ripasso2Exception.java
+++ src/esercizi_in_lezione/week8/day5/Ripasso2Exception.java
@@ -1,14 +1,23 @@
 package esercizi_in_lezione.week8.day5;
 
 public class Ripasso2Exception {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MyException {
         // TODO: fai una divisione per zero
         // cattura l'eccezione e lancia manualmente una nuova eccezione MyException
         // stampa il messaggio d'errore
+        try {
+           int x = 10/0;
+        }
+        catch (Exception e){
+            throw new MyException("Divisione per 0");
+        }
     }
 }
 
 // TODO: definire un'eccezione personalizzata
 class MyException extends Exception {
     // TODO:
+    public MyException (String message){
+        super (message);
+    }
 }
