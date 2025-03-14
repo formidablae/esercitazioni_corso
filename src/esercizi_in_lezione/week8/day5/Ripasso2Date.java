7837bde1d229741c3bb16711ae279dc4b2854b65
paolo9688 <paolo.pz1@gmail.com>
2025-03-14 14:40:38 +0100

    risolto parte esercizio pp

--- src/esercizi_in_lezione/week8/day5/Ripasso2Date.java
+++ src/esercizi_in_lezione/week8/day5/Ripasso2Date.java
@@ -1,9 +1,18 @@
 package esercizi_in_lezione.week8.day5;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
 public class Ripasso2Date {
     public static void main(String[] args) {
         // TODO: Stampare la data corrente
-
+        LocalDate dataCorrente = LocalDate.now();
+        System.out.println(dataCorrente);
         // TODO: Stampare l'ora di adesso in Tokyo
+        String timeZone = "Asia/Tokyo";
+        ZoneId zoneId = ZoneId.of(timeZone);
+        ZonedDateTime zonedDateTime = ZonedDateTime.now(zoneId);
+        System.out.println(zonedDateTime);
     }
 }
