8ec9a979e47fce8f15c77e3efd0aee6784261de4
Dennis Ciccone <dennisciccone02@gmail.com>
2025-03-14 14:56:53 +0100

    Svolto esercizio Interface ripasso2

--- src/esercizi_in_lezione/week8/day5/Ripasso2Interface.java
+++ src/esercizi_in_lezione/week8/day5/Ripasso2Interface.java
@@ -1,16 +1,37 @@
 package esercizi_in_lezione.week8.day5;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Ripasso2Interface {
     public static void main(String[] args) {
         // TODO: Usare il metodo sort
+        OrdinamentoArray ordinaArray = new OrdinamentoArray();
+
+        ArrayList<String> array = new ArrayList<>();
+        array.add("1");
+        array.add("2");
+        array.add("3");
+
+        ArrayList<String> arrayOrdinato = ordinaArray.sort(array);
+        System.out.println(arrayOrdinato);
     }
 }
 
 // TODO: scrivere un'interfaccia che definisce un metodo che prende in input un array di stringhe e ritorna un array di stringhe ordinato
-interface ArrayToString {
-    String[] sort(String[] array);
+interface OrdinaArray {
+    ArrayList<String> sort(ArrayList<String> array);
 }
 
 // TODO: scrivere una classe che implementa l'interfaccia e definisce il metodo
+
+class OrdinamentoArray implements OrdinaArray {
+    @Override
+    public ArrayList<String> sort(ArrayList<String> array) {
+        Collections.sort(array);
+        return array;
+    }
+}
 
 // TODO: scrivere un test sul metodo sort
