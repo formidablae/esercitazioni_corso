7ae25aa2616fe9b27d3819a2d9181ed8bb8f025e
paolo9688 <paolo.pz1@gmail.com>
2025-03-14 15:04:56 +0100

    risolto esercizio pp

--- src/esercizi_in_lezione/week8/day5/Ripasso2LinkedList.java
+++ src/esercizi_in_lezione/week8/day5/Ripasso2LinkedList.java
@@ -1,13 +1,23 @@
 package esercizi_in_lezione.week8.day5;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Ripasso2LinkedList {
     public static void main(String[] args) {
         // TODO: scrivere una struttura dati con la catena alimentare:
         // uccello -> serpente -> topo -> erba
         // Stampa la catena alimentare
+        List<String> catenaAlimentare = Arrays.asList("uccello", "serpente", "topo", "erba");
+        System.out.println(catenaAlimentare);
+        List<String> linkedCatenaAlimentare = new LinkedList<>(catenaAlimentare);
 
         // TODO: inserire un nuovo elemento in cima alla catena alimentare ad esempio: lupo
+        linkedCatenaAlimentare.addFirst("lupo");
 
         // TODO: inserire un nuovo elemento tra topo e erba ad esempio: formica
+        linkedCatenaAlimentare.add(4, "formica");
+        System.out.println(linkedCatenaAlimentare);
     }
 }
